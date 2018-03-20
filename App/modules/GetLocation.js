c4a4c5f512fa659697b7d9b1d47676d15242fccc
jasonh9 <jhuang9@mail.sfsu.edu>
2018-03-19 21:15:53 -0700

    implemented a test navigator.geolocation.getCurrentLocation method

---
+++ App/modules/GetLocation.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class GeolocationExample extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
+  render() {
+    return (
+        <Text>
+            Latitude: { this.state.latitude }{`\n`}
+            Longitude: { this.state.longitude }
+        </Text>
+    );
+  }
+}
+
+export default GeolocationExample;
